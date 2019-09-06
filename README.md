3906863f52ebea20bfbf1708dbb5c87be5638afa
Ethan Manarin <35640579+kindanon@users.noreply.github.com>
2019-09-06 19:34:50 -0400

    simple & complicated version
    simple is dumb and works
    complex is weird and does not

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
 # fortnite_afk_tool
 simple python script to afk grind on fortnite
 
-//TODO \
-figure out why windows rgb values arn't uniform on different machines \
-target window to remove resolution requirement
+DONT EXPECT THIS CODE TO WORK
 
----HOW TO RUN--- \
-must have python installed \
-expect to tweak rgb values \
-Fortnite must be run fullscreen/borderless window a 1080p monitor \
+Fortnite must be run fullscreen a 1080p monitor
 if you have multiple monitors, then fortnite must be on the left-most one
--- fortnite_afk.py
+++ fortnite_afk.py
@@ -5,14 +5,67 @@ import time
 
 mouse = m_cont()
 keyboard = k_cont()
+
+#coordinates of buttons / icons
+c_start = (1600,950)
+c_bus = (1635,325)
+c_dead = (235,95)
+c_escape = (1820,1050)
+c_survey = (500,755)
+c_news = (655, 940)
+c_item = (476,882)
+c_items = (615,882)
+c_list = [c_start, c_bus, c_dead, c_escape, c_survey, \
+c_news, c_item, c_items]
+
+#color of button / icons
+rgb_start = (245,  232, 72)
+rgb_bus = (101,  159, 55)
+rgb_dead = (255,  0, 22)
+rgb_escape = None
+rgb_survey = (157,195,255)
+rgb_news = (255,255,255)
+rgb_item = (255,  255, 255)
+rgb_items = (255,  255, 255)
+rgb_list = [rgb_start, rgb_bus, rgb_dead, rgb_escape, rgb_survey, \
+rgb_news, rgb_item, rgb_items]
+
+img = ImageGrab.grab(bbox = (0,0,1920,1080))
+
+for i_coord, coord in enumerate(c_list):
+	pixel = img.getpixel(coord)
+	
+	for i_rgb, rgb in enumerate(rgb_list):
+		if pixel == rgb
+			actions(i_rgb)
+
+			
+def actions(case):
+	switcher = {
+		0: start,
+		1: bus,
+		2: dead,
+		3: escape,
+		4: survey,
+		5: news,
+		6: item,
+		7: items,
+	}
+	func = switcher.get(i)
+	return func()
+
+
 hc = (1600,950)
 bc = (1635,325)
 ec = (235,95)
 ic = (476,882)
 mc = (615,882)
 sc = (500,755)
-
+nc = (655, 940)
+	
 while True:
+	
+	#mouse.position = hc
 	#start infinite loop
 	time.sleep(3)
 	
@@ -31,9 +84,10 @@ while True:
 	item = img.getpixel(ic)
 	items = img.getpixel(mc)
 	survey = img.getpixel(sc)
+	news = img.getpixel(nc)
 	
 	#if on home
-	if home == (245,  231, 72):
+	if home == (245,  232, 72):
 		mouse.position = hc
 		mouse.click(Button.left, 1)
 		time.sleep(.5)
@@ -62,6 +116,13 @@ while True:
 	#if survey
 	elif survey == (157,195,255):
 		mouse.position = sc
+		mouse.click(Button.left, 1)	
+		time.sleep(.5)
+		mouse.position = (0,0)
+	
+	#if news
+	elif news == (255,255,255):
+		mouse.position = nc
 		mouse.click(Button.left, 1)	
 		time.sleep(.5)
 		mouse.position = (0,0)
